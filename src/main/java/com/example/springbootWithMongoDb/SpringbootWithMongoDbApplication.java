d1041b87f05f80e007532221b1668bd7ff957835
Faisal Aleissa <faleissa@stcpay.com.sa>
2021-12-15 23:59:25 +0300

    first commit

---
+++ src/main/java/com/example/springbootWithMongoDb/SpringbootWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootWithMongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithMongoDbApplication.class, args);
+	}
+
+}
