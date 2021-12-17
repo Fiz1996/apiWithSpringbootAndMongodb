2ebb0c2e359108a86ebcd5e3bd6f416bcbb5ac21
Faisal Aleissa <mr.faisal.aleissa@gmail.com>
2021-12-17 14:45:02 +0300

    finished implementing servcie and controller

--- src/main/java/com/example/springbootWithMongoDb/SpringbootWithMongoDbApplication.java
+++ src/main/java/com/example/springbootWithMongoDb/SpringbootWithMongoDbApplication.java
@@ -23,54 +23,8 @@ public class SpringbootWithMongoDbApplication {
 		SpringApplication.run(SpringbootWithMongoDbApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
-		return args -> {
-			Address address = new Address(
-					"England",
-					 "London",
-					"NE9"
-
-			);
-
-			String email = "fadsfsa@email.com";
-			Student student = new Student(
-					"Ahmad",
-					"test",
-					email,
-					Gender.MALE,
-					address,
-					List.of("Computer Science","Maths"),
-					BigDecimal.TEN,
-					LocalDateTime.now()
-			);
 
 
-		//	usingMongoDbTemplate(repository, mongoTemplate, email, student);
-			repository.findStudentByEmail(email)
-			.ifPresentOrElse(student1 -> {
-				System.out.println(student + "already exists");
 
-			},()-> {			System.out.println("Inserting student"+ student);
-				repository.insert(student);});
-
-		};
-	}
-
-	private void usingMongoDbTemplate(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
-		Query query = new Query();
-		query.addCriteria(Criteria.where("email").is(email));
-		List<Student> students= mongoTemplate.find(query,Student.class);
-		if(students.size() >1){
-			throw new IllegalArgumentException("found many students with email "+ email);
-		}
-		if(students.isEmpty()){
-			System.out.println("Inserting student"+ student);
-			repository.insert(student);
-		}
-		else {
-			System.out.println(student + "already exists");
-		}
-	}
 
 }
